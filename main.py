a0200f1e62cfd473e299c222db42feb1393d329b
Sarah B <barker.sarah.ms@gmail.com>
2024-05-23 17:19:46 -0400

    idk if i'm doing this right... but...let's try

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("Welcome to GC mini golf!")
+name = input("What is your name?")
+print("Hi" + name)
+holes = [3, 6]
+input("Would you like to play 3 or 6 holes today?")
+print(holes[3])  # How many putts for hole 1?
+
+
