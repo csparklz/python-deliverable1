a2c6294bdfa4f8631f6b48231b90f2ebdac2e1c8
Sarah B <barker.sarah.ms@gmail.com>
2024-05-25 14:42:08 -0400

    idk if i'm doing this right... but...let's try

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ for hole in range(1, num_holes + 1):
         try:
             num_putts = int(input(f"How many putts for hole {hole}?) (par is 3) ").strip())
             putts.append(num_putts)
+            running_total = sum(putts)
+            print(f"Current running total: {running_total}")
             break
         except ValueError:
             print("Invalid input. Please enter a number.")
@@ -32,12 +34,12 @@ score_difference = total_putts - par_score
 
 if total_putts > par_score:
     over_par = total_putts - par_score
-    print(f"Nice try, {name}! Your total score was +{score_difference}.")
+    print(f"Nice try, {name}! Your total par was +{score_difference}.")
 elif total_putts < par_score:
     under_par = par_score - total_putts
-    print(f"Great job, {name}! Your total score was {score_difference}.")
+    print(f"Great job, {name}! Your total par was {score_difference}.")
 else:
-    print(f"Good game, {name}! Your total score was: 0.")
+    print(f"Good game, {name}! Your total par was: 0.")
 
 
 
